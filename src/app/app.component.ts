ffa86532016f5f080274bbd788a6452a36dac45a
Greg <gregory82gr@gmail.com>
2019-09-27 12:01:33 +0300

    03-a-toh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,12 @@ export class Hero {
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  template: `<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>`
+  template: `
+  <h1>{{title}}</h1>
+  <h2>{{hero.name}} details!</h2>
+  <div><label>id: </label>{{hero.id}}</div>
+  <div><label>name: </label>{{hero.name}}</div>
+  `
 })
 export class AppComponent {
   title = 'Tour of Heroes';
